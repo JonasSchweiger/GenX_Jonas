da701ed14f056a7a61698eb084a38a24c3c3dc41
JonasSchweiger <jonas.schweiger@epfl.ch>
2024-08-05 14:51:45 -0400

    try out

--- example_systems/1_three_zones/Run.jl
+++ example_systems/1_three_zones/Run.jl
@@ -1,4 +1,12 @@
 using GenX
 using Gurobi
+using CSV
+using DataFrames
+using VegaLite
+using PyPlot
+using Plots
+pyplot()
 
-run_genx_case!(dirname(@__FILE__), Gurobi.Optimizer)
+#run_genx_case!(dirname(@__FILE__), Gurobi.Optimizer)
+
+power =  CSV.read("example_systems/1_three_zones/results_4/power.csv",DataFrame,missingstring="NA")
