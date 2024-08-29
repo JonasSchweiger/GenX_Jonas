aab1d6896a31066e6fa4d574087b4b23cfe712bb
JonasSchweiger <jonas.schweiger@epfl.ch>
2024-08-29 13:41:10 -0400

    15min methanol

---
+++ backup_systems/01_one_zone_perfect_foresight_15min/Visualization.jl
@@ -0,0 +1,21 @@
+using DataFrames
+
+
+case = joinpath("backup_systems/01_one_zone_perfect_foresight_15min");
+emm1 =  CSV.read(joinpath(case,"results/emissions.csv"),DataFrame)
+
+# Pre-processing
+tstart = 1
+tend = 400
+names_emm = ["Zone 1"] 
+
+emm_tot = DataFrame([emm1[3:end,2]],  # Only take the first column of emm1
+    ["Zone 1"])
+
+emm_plot = DataFrame([collect((tstart-3):(tend-3)) emm_tot[tstart:tend,1] repeat([names_emm[1]],(tend-tstart+1))],
+    ["Hour","MW","Zone"]) 
+
+    emm_plot  |>
+@vlplot(mark={:line},
+    x={:Hour,title="Time Step (hours)",labels="Zone:n",axis={values=tstart:24:tend}}, y={:MW,title="Emmissions (Tons)",type="quantitative"},
+    color={"Zone:n"},width=845,height=400,title="Emmissions per Time Step by Zone")
