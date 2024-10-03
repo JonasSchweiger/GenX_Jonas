139903258c44efa408ae457aa2d6014aa8d4d65c
JonasSchweiger <jonas.schweiger@epfl.ch>
2024-10-03 11:49:43 -0400

    secondary batteries restrictions

--- backup_systems/03_real_demand_sec_batteries/Run.jl
+++ backup_systems/03_real_demand_sec_batteries/Run.jl
@@ -62,11 +62,11 @@ fuels = myinputs["fuels"]
 fuel_costs = myinputs["fuel_costs"]
 omega = myinputs["omega"]
 END_SUBPERIODS = myinputs["START_SUBPERIODS"] .+ myinputs["hours_per_subperiod"] .-1
-EMERGENCY_PURCHSASE_TIME = 1:480:T
+EMERGENCY_PURCHSASE_TIME = 1:24:T
 
 no_purchases = Int64[]
 for r in gen
-    if gen.backup_emergency_purchase == 0
+    if r.backup_emergency_purchase == 0
         push!(no_purchases, r.id)
     end
 end
