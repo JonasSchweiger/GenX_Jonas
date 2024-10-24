54c8e94695c4d249ca3f483a0ae9b4adcef3ed53
JonasSchweiger <jonas.schweiger@epfl.ch>
2024-10-24 10:54:10 -0400

    sensitivity analysis

--- backup_systems/03_real_demand_sec_batteries/Run_SA.jl
+++ backup_systems/03_real_demand_sec_batteries/Run_SA.jl
@@ -14,11 +14,19 @@ using Plots
 using Statistics
 
 # Function to modify Thermal.csv with a given capacity constraint and technology
-function modify_thermal_csv(capacity_constraint, technology)
+function modify_thermal_csv_all(capacity_constraint, technology)
     df = CSV.read("backup_systems/03_real_demand_sec_batteries/resources/Thermal.csv", DataFrame)
-    df[!, "Max_Cap_MW"] .= 0  # Reset all "Max_Cap_MW" values
+    df[!, "Max_Cap_MW"] .= -1.0  # Reset all "Max_Cap_MW" values
     df[df.Resource .== technology, "Max_Cap_MW"] .= capacity_constraint # Set for the specified technology
     CSV.write("backup_systems/03_real_demand_sec_batteries/resources/Thermal.csv", df)
+end
+
+
+function read_capacity_csv(filename)
+    df = CSV.read(filename, DataFrame)
+    # Remove leading/trailing whitespace and non-numeric characters
+    df[!, :CapacityConstraintDual] = tryparse.(Float64, replace.(df[!, :CapacityConstraintDual], r"[^0-9\.\-]" => "")) 
+    return df
 end
 
 # ---  Specify the technology here ---
@@ -34,16 +42,16 @@ capacity_constraints = range(0.1, stop=2, length=15)
 # Iterate over the capacity constraints
 for capacity_constraint in capacity_constraints
     # Modify Thermal.csv for the selected technology
-    modify_thermal_csv(capacity_constraint, technology)
+    modify_thermal_csv_all(capacity_constraint, technology)
 
     # Run the model
     include("Run.jl")
 
     # Read the Capacity_Constraint_Dual value from capacity.csv
-    dfCapacity = CSV.read("backup_systems/03_real_demand_sec_batteries/results/capacity.csv", DataFrame)
+    dfCapacity = read_capacity_csv("backup_systems/03_real_demand_sec_batteries/results/capacity.csv")
     
     # Extract dual value using filter and column selection
-    capacity_dual_value = filter(:Resource => ==(technology), dfCapacity)[:, :Capacity_Constraint_Dual][1] 
+    capacity_dual_value = filter(:Resource => ==(technology), dfCapacity)[:, :CapacityConstraintDual][1] 
 
     # Add the results to the DataFrame
     push!(dfResults, [capacity_constraint, capacity_dual_value])
