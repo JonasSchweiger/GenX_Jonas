03112bc6e57398d1f210c751fb90c5f41e78f05f
JonasSchweiger <jonas.schweiger@epfl.ch>
2024-11-12 18:51:14 -0500

    div

--- backup_systems/03_real_demand_sec_batteries/Run.jl
+++ backup_systems/03_real_demand_sec_batteries/Run.jl
@@ -111,7 +111,7 @@ end
 @expression(EP, eBackup_EReplacement[y in myinputs["SINGLE_FUEL"]], GenX.backup_replacement_factor(gen[y]) * vBackup_fuel_capacity[y] * fuel_CO2[GenX.fuel(gen[y])]) #MMBtu * tCO2/MMBtu = tCO2
 @expression(EP, eBackup_Total_EReplacement, sum(EP[:eBackup_EReplacement][y] for y in 1:G))
 
-@constraint(EP, cBackup_Total_Emissions, EP[:eBackup_Total_EReplacement] <= 6.352674) # GenX.emergency_quantity_mmbtu(gen[8])) #value.(myinputs["dfMaxCO2"])
+@constraint(EP, cBackup_Total_Emissions, EP[:eBackup_Total_EReplacement] <= 20000) # GenX.emergency_quantity_mmbtu(gen[8])) #value.(myinputs["dfMaxCO2"])
 
 #EP[:cCO2Emissions_systemwide] += eBackup_Total_EReplacement
 
